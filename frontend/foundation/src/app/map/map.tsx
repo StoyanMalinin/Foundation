f57dd877c916722d628fdb60d80e88d39bc29bb8
StoyanMalinin <stoyanmalinin2002@gmail.com>
2025-03-09 23:55:19 +0200

    optimize map

--- frontend/foundation/src/app/map/map.tsx
+++ frontend/foundation/src/app/map/map.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useEffect, useReducer, useRef, useState } from 'react';
-import MapTile from './map-tile';
 import { boundingBoxToTileGrid } from './map-utils'
-import * as utils from './map-utils'
 import * as tomtom from './tomtom-map-utils'
 
 type MapProps = {
@@ -11,54 +9,63 @@ type MapProps = {
     lon: [number, number],
     drawWidth: number,
     drawHeight: number,
+    canvasRef: React.RefObject<HTMLCanvasElement>,
 };  
 
-const tiles: Record<string, string> = {};
-
 export default function Map(props: MapProps) {
+    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+    const [tiles, setTiles] = useState<Record<string, HTMLImageElement>>({});
+
     const grid = boundingBoxToTileGrid(props.lat[0], props.lat[1], props.lon[0], props.lon[1]);
     const rows = grid.length;
     const cols = grid[0].length;
-    const canvasRef = useRef(null);
-    const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
-    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
-    const [, forceUpdate] = useReducer(x => x + 1, 0);
 
     useEffect(() => {
-        if (canvasRef?.current) {
-            const _canvas = canvasRef?.current as HTMLCanvasElement;
-            setCtx(_canvas.getContext("2d"));
+        if (props.canvasRef.current) {
+            console.log("Setting canvas");
+
+            const _canvas = props.canvasRef.current;
             setCanvas(_canvas);
         }
-    }, []);
+    }, [props.canvasRef]);
 
+    // Fix this shit:
+    // 1. We essentially copy all the tiles into the `tiles` state property (maybe I don't use React properly)
+    //   - We just need something to indicate which tiles are loaded and which are not 
+    // 2. Too much async/await stuff - should be simpler (same argument about react as before)
     useEffect(() => {
         for (let i = 0; i < rows; i++) {
             for (var j = 0; j < cols; j++) {
                 const f = async function() {
                     if (!tiles[JSON.stringify(grid[i][j])]) {
                         tiles[JSON.stringify(grid[i][j])] = await fetchMapTile(grid[i][j].z, grid[i][j].x, grid[i][j].y);
-                        forceUpdate();
+                        setTiles(tiles);
                     }
-                }
+                };
 
                 f();
             }
         }
-    })
+    });
 
-    if (ctx && canvas) {
-        ctx?.clearRect(0, 0, canvas.width, canvas.height);
-        console.log("grid-dump", grid, rows, cols);
+    if (props.canvasRef.current) {
+        props.canvasRef.current.width = props.drawWidth;
+        props.canvasRef.current.height = props.drawHeight;
+    }
+
+    if (canvas) {
+        console.time("Render");
+
+        const ctx = canvas.getContext("2d");
+        if (ctx == null) {
+            throw "Failed to get 2d context";
+        }
 
         for (let i = 0; i < rows; i++) {
             for (var j = 0; j < cols; j++) {
                 if (!tiles[JSON.stringify(grid[i][j])]) {
                     continue;
-                }
-
-                if (!grid[i][j]) {
-                    console.log("grid render", i, j, rows, cols);
                 }
     
                 const box = tomtom.tileZXYToLatLonBBox(grid[i][j].z, grid[i][j].x, grid[i][j].y);
@@ -92,29 +99,36 @@ export default function Map(props: MapProps) {
                 const sHeight = sHeightFraction * 256;
                 const dWidth = dWidthFraction * width;
                 const dHeight = dHeightFraction * height;
-            
-                const ctx = canvas.getContext("2d");
-    
-                const img = new Image();
-                img.src = tiles[JSON.stringify(grid[i][j])];
-
-                console.log(cols, rows, dx1Fraction, dy1Fraction, sx1Fraction, sy1Fraction, props, lonBox);
                 
-                img.onload = () => ctx?.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+                ctx.drawImage(tiles[JSON.stringify(grid[i][j])], sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
             }
         }
+
+        console.timeEnd("Render");
     }
 
-    return <canvas id="map-canvas" ref={canvasRef} width={props.drawWidth} height={props.drawHeight} style={{width: "100%", height: "100%", objectFit: "contain"}}></canvas>
+    return <></>
 }
 
-async function fetchMapTile(z: number, x: number, y: number): Promise<Image> {
-    console.log("FETCHING", z, x, y);
+const tileCache: Record<string, HTMLImageElement> = {};
+async function fetchMapTile(z: number, x: number, y: number): Promise<HTMLImageElement> {
+    if (tileCache[`${z},${x},${y}`]) {
+        return tileCache[`${z},${x},${y}`];
+    }
+    
     var url = `http://localhost:6969/map-tile?searchId=${1}&z=${z}&x=${x}&y=${y}`;
 
     const response = await fetch(url);
     if (response.status == 200) {
-        return URL.createObjectURL(await response.blob());
+        const img = new Image();
+        img.src = URL.createObjectURL(await response.blob());
+
+        const p = new Promise<void>((resolve, reject) => {
+            img.onload = () => resolve();
+        });
+        await p;
+        
+        return img;
     } else {
         throw response.statusText;
     }
--- frontend/foundation/src/app/map/page.tsx
+++ frontend/foundation/src/app/map/page.tsx
@@ -1,93 +1,18 @@
 "use client";
 
-import dynamic from 'next/dynamic'
-import { useRef, useState, useEffect } from "react"
-import Map from "./map"
-import { eventNames } from "process";
-import { deleteAppClientCache } from "next/dist/server/lib/render-server";
-import internal from 'stream';
+import { useRef } from "react";
+import MapWrapper from "./map-wrapper";
 
-function clipBetween(x: number, l: number, r: number): number {
-  return Math.min(Math.max(x, l), r);
-}
+export default function MapPage() {
+    console.log("Render MapPage");
 
-type LatLon = {
-  lat: [number, number],
-  lon: [number, number],
-}
+    const divRef = useRef<HTMLDivElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-export default function MapWrapper() {
-    const [mousePosition, setMousePosition] = useState<{x : number | null, y : number | null}>({ x: null, y: null });
-    const divRef = useRef(null);
-
-    const [latLon, setLatLon] = useState<LatLon>({lat: [-80, +80], lon: [-170, +170]});
-    const [isMouseUp, setMouseUp] = useState<boolean>(true);
-    const [size, setSize] = useState<[number, number]>([0, 0]); // [width, height]
-
-    useEffect(() => {
-      if (divRef?.current) {
-        console.log("set size");
-        const bounding = divRef?.current?.getBoundingClientRect();
-        setSize([bounding.width, bounding.height]);
-      }
-    }, []);
-
-    console.log("size", size[0], size[1]);
-    const handleMouseMove = (event: MouseEvent) => {      
-        if (!divRef?.current) {
-          return;
-        }
-
-        if (isMouseUp) {
-          if (mousePosition.x != null) {
-            setMousePosition({x: null, y: null});
-          }
-
-          return;
-        }
-
-        setMousePosition({x: event.clientX, y: event.clientY});
-
-        if (mousePosition.x != null && mousePosition.y != null) {
-            const [width, height] = size;
-            const offsetX = (event.clientX - mousePosition.x);
-            const offsetY = (event.clientY - mousePosition.y);
-
-            const newLat: [number, number] = [0, 0];
-            newLat[0] = latLon.lat[0] + (offsetY / height) * (latLon.lat[1] - latLon.lat[0]);
-            newLat[1] = latLon.lat[1] + (offsetY / height) * (latLon.lat[1] - latLon.lat[0]);
-            
-            const newLon: [number, number] = [0, 0];
-            newLon[0] = latLon.lon[0] - (offsetX / width) * (latLon.lon[1] - latLon.lon[0]);
-            newLon[1] = latLon.lon[1] - (offsetX / width) * (latLon.lon[1] - latLon.lon[0]);
-            
-            setLatLon({
-              lat: newLat,
-              lon: newLon
-            });
-        }        
-      };
-
-      const handleMouseWheel = (event: WheelEvent) => {
-        if (!divRef?.current) {
-          return;
-        }
-
-        if (Math.abs(event.deltaY) > 1) {
-          const latDelta = latLon.lat[1] - latLon.lat[0];
-          const lonDelta = latLon.lon[1] - latLon.lon[0];
-
-          const scale = Math.exp(event.deltaY / 5000.0);
-          
-          const newLatDelta = clipBetween(latDelta * scale, 0.5, 180);
-          const newLonDelta = clipBetween(lonDelta * scale, 0.5, 360);
-
-          setLatLon({
-            lat: [latLon.lat[0] + (latDelta - newLatDelta) / 2, latLon.lat[1] - (latDelta - newLatDelta) / 2],
-            lon: [latLon.lon[0] + (lonDelta - newLonDelta) / 2, latLon.lon[1] - (lonDelta - newLonDelta) / 2]
-          });
-        }
-      };
-
-    return <div ref={divRef} key={"map-wrapper-key"} onMouseMove={handleMouseMove} onMouseDown={() => setMouseUp(false)} onMouseUp={() => setMouseUp(true)} onWheel={handleMouseWheel} className="map-wrapper"><Map lat={latLon.lat} lon={latLon.lon} drawWidth={size[0]} drawHeight={size[1]} /></div>
+    return (
+        <div ref={divRef} key={"map-wrapper-key"} className="map-wrapper">
+            <canvas id="map-canvas" ref={canvasRef} style={{width: "100%", height: "100%", objectFit: "contain"}} />
+            <MapWrapper divRef={divRef} canvasRef={canvasRef} />        
+        </div>
+    );
 }
