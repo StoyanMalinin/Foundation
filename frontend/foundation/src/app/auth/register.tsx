2125a7366e3a914588392b424671453bf3db9306
StoyanMalinin <stoyanmalinin2002@gmail.com>
2025-08-15 21:24:14 +0300

    Add register form

---
+++ frontend/foundation/src/app/auth/register.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Button, Input } from "@mui/material";
+import { redirect } from "next/navigation";
+import Form from "next/form";
+import { useActionState } from "react";
+
+type FormState = {
+    username: string
+    errorMessage: string,
+}
+
+export default function Register() {
+    const [formState, formAction] = useActionState(registerAction, {
+        username: "",
+        errorMessage: "",
+    });
+
+    const errorText = (<label style={{color: "red"}}>{formState.errorMessage}</label>)
+    return <Form action={formAction}>
+        <h2>Register</h2>
+        <Input name="username" placeholder="Username" defaultValue={formState.username} type="text" /><br></br>
+        <Input name="password" placeholder="Password" type="password" /><br></br>
+        <Input name="first_name" placeholder="First name" type="text" /><br></br>
+        <Input name="last_name" placeholder="Last name" type="text" /><br></br>
+        
+        <br></br>
+        <Button type="submit" variant="contained">Register</Button>
+
+        {formState.errorMessage != "" && <><br/><br/>{errorText}</>}
+    </Form>
+}
+
+async function registerAction(prevState: FormState, formData: FormData): Promise<FormState> {
+    let res = await fetch('https://localhost:6969/register', {
+        method: "post",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            'username': formData.get("username"),
+            'password': formData.get("password"),
+            'first_name': formData.get("first_name"),
+            'last_name': formData.get("last_name"),
+        }),
+        credentials: "include",
+    });
+
+    if (res.status == 200) {
+        redirect('/');
+    }
+
+    return {
+        username: formData.get("username")?.toString() ?? "",
+        errorMessage: await res.text(),
+    };
+}
